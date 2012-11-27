06ed70de0f970b63ed90f4e78075bc2e732213f1
Dax Ponce de Leon <daxitude@gmail.com>
2012-11-26 20:28:44 -0600

    added navigation up/down/enter on the autocomplete list

--- adminquicksearch.js
+++ adminquicksearch.js
@@ -13,6 +13,8 @@ jQuery(document).ready(function ($) {
 		this.url = options.url;
 		this.oldVal = '--';
 		this.appended = false;
+		this.showing = false;
+		this.$active = false;
 		this.$list;
 		this.initialize();
 	}
@@ -26,22 +28,75 @@ jQuery(document).ready(function ($) {
 		},
 		// on focus callback
 		activate: function () {
+			$(window).on('keyup.saList', $.proxy(this.navigate, this));
 			if (this.appended) return;
 			this.$list = $('<ul class="adminqs-list ab-submenu"></ul>');
-			this.$list.insertAfter(this.$input.parent()).wrap('<div class="ab-sub-wrapper"></div>').parent().hide();
+			this.$list
+				.insertAfter(this.$input.parent())
+				.wrap('<div class="ab-sub-wrapper"></div>')
+				.parent().hide();
 			this.appended = true;
 		},
 		// on blur callback
 		destroy: function () {
 			var self = this;
+			$(window).off('keyup.saList')
 			window.setTimeout(function () {
 				self.$list.parent().hide();
+				self.showing = false;
+				self.$active = false;
 			}, 125);
+		},
+		navigate: function (e) {
+			console.log(e.keyCode)
+			if (!this.showing) return;
+			var kids = this.$list.children();
+			switch (e.keyCode) {
+				case 27: // esc
+					this.destroy();
+					break;
+				case 38: // up
+					e.preventDefault();
+					this.prev();
+					break;
+				case 40: // down
+					e.preventDefault();	
+					this.next();
+					break;
+				case 13: // enter
+					e.preventDefault();
+					if (this.$active)
+						window.location.href = this.$active.children('a').attr('href');
+					break;
+				default:
+			}
+		},
+		prev: function () {
+			var prev = this.$active ? this.$active.prev() : '';
+			
+			if (!prev.length)
+				prev = this.$list.children().last();
+			
+			this.$active && this.$active.removeClass('active');
+			prev.addClass('active');
+			this.$active = prev;
+		},
+		next: function () {
+			var next = this.$active ? this.$active.next() : '';
+			
+			if (!next.length)
+				next = this.$list.children().first();
+			
+			this.$active && this.$active.removeClass('active');
+			next.addClass('active');
+			this.$active = next;
 		},
 		// on keyup callback
 		change: function (e) {
 			var val = $(e.currentTarget).val();
-			if ( val.length < 2 ) return;	
+			if ( val.length < 2 || val.length === this.oldVal.length ) return;
+			this.showing = true;
+			this.$active = false;
 			this.oldVal = val;
 			this.$list.parent().show();
 			this.fetch(val);
@@ -62,7 +117,7 @@ jQuery(document).ready(function ($) {
 			data = JSON.parse(data);
 
 			if ( !data.length )
-				items = itemli({title: 'No items found'});
+				items = itemli({title: 'No items found', link: 'javascript:void(0)'});
 			else
 				$.each(data, function (index, elem) { items += itemli(elem); });
 
